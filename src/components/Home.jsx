25f5c06273fb9926906b3fd5e45763a055480b5b
Santiago Velosa <santiagovelosa@gmail.com>
2022-02-01 21:10:50 -0500

    Fetch and render info on Home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchData } from '../redux/home/home';
 
@@ -6,13 +6,20 @@ const Home = () => {
   const data = useSelector((state) => state.home.data);
   const dispatch = useDispatch();
 
-  if (data.length === 0) {
-    dispatch(fetchData());
-  }
+  useEffect(() => {
+    if (data.length === 0) {
+      dispatch(fetchData());
+    }
+  }, []);
 
   return (
     <div>
       <p>This is the home page!</p>
+      <ul>
+        {data.map((item) => (
+          <li key={item.id}>{item.year}</li>
+        ))}
+      </ul>
     </div>
   );
 };
--- src/redux/home/home.js
+++ src/redux/home/home.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
-const FETCH_DATA = 'React-Capstone/src/redux/home/FETCH_DATA';
-const API_KEY = '2a09e3ba2d6d10784b25d56655b5b5ae';
+const FETCH_HOME_DATA = 'React-Capstone/src/redux/home/FETCH_HOME_DATA';
+// const API_KEY = '2a09e3ba2d6d10784b25d56655b5b5ae';
+// const API_KEY = 'ddd657ad54ceb10b1486a984f1eb30ac';
+const API_KEY = '77df038e719b3f3653b2467abd2e7baf';
 const URL = `https://financialmodelingprep.com/api/v3/income-statement/AAPL?limit=120&apikey=${API_KEY}`;
 // const companies = ['AAPL', 'MSFT', 'GOOGL', 'AMZN', 'COKE', 'TSLA', 'BRK-B', 'SPOT'];
 const initalState = {
@@ -10,22 +12,26 @@ const initalState = {
 
 export const fetchData = () => async (dispatch) => {
   const response = (await axios.get(URL)).data;
-  console.log(response);
 
   const info = [];
-  // Object.keys(response).map((object) => (
-  //   info.push(object)
-  // ));
+  Object.entries(response).map((item) => {
+    const fetchedInfo = {
+      year: item[1].calendarYear,
+      id: item[1].date,
+    };
+    info.push(fetchedInfo);
+    return info;
+  });
 
   dispatch({
-    type: FETCH_DATA,
+    type: FETCH_HOME_DATA,
     payload: info,
   });
 };
 
 const home = (state = initalState, action) => {
   switch (action.type) {
-    case FETCH_DATA:
+    case FETCH_HOME_DATA:
       return {
         ...state,
         data: action.payload,
