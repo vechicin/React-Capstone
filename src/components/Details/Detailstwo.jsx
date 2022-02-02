44f698b1a42128e582d92459a73a1d538eff1efd
Santiago Velosa <santiagovelosa@gmail.com>
2022-02-02 11:41:39 -0500

    Finish Home page logic

---
+++ src/components/Details/Detailstwo.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Detailstwo = () => (
+  <p>This is the second details page!</p>
+);
+
+export default Detailstwo;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { NavLink } from 'react-router-dom';
 import { fetchData } from '../redux/home/home';
 
 const Home = () => {
@@ -17,7 +18,11 @@ const Home = () => {
       <p>This is the home page!</p>
       <ul>
         {data.map((item) => (
-          <li key={item.id}>{item.year}</li>
+          <li key={item.id}>
+            <NavLink to={`details/${item.id}`}>
+              {item.year}
+            </NavLink>
+          </li>
         ))}
       </ul>
     </div>
