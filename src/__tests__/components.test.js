6c625de01524a39cc6a34d788ba125389661a2b7
Santiago Velosa <santiagovelosa@gmail.com>
2022-02-03 19:42:12 -0500

    Finish tests

--- src/__tests__/components.test.js
+++ src/__tests__/components.test.js
@@ -1,12 +1,26 @@
 import { render } from "@testing-library/react"
 import { Provider } from "react-redux"
+import { Router } from "react-router"
 import { PersistGate } from "redux-persist/integration/react"
+import Details from "../components/Details"
 import Home from "../components/Home"
+import Navbar from "../components/Navbar"
+import Statements from "../components/Statements"
 import store, { persistor } from "../redux/configureStore"
 
 describe('Components testing', () => {
   test('Home component', () => {
-    const tree = render(<Provider store={store}><PersistGate persistor={persistor}><Home /></PersistGate></Provider>);
+    const tree = render(<Provider store={store}><Home /></Provider>);
+    expect(tree).toMatchSnapshot();
+  });
+
+  test('Details component', () => {
+    const tree = render(<Provider store={store}><Details /></Provider>);
+    expect(tree).toMatchSnapshot();
+  });
+
+  test('Statements component', () => {
+    const tree = render(<Provider store={store}><Statements /></Provider>);
     expect(tree).toMatchSnapshot();
   });
 });
